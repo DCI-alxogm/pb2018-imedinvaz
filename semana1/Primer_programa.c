00cf79a3d03385a67e2e93ad2af46c442e57e00b
Medina Vazquez Irving <medinavi2018@dci25-187.ugto.mx>
2018-08-08 09:38:56 -0500

    cree el primer programa

---
+++ semana1/Primer_programa.c
@@ -0,0 +1,10 @@
+/* creado el8 de agosto de 2018 por irving*/
+
+#include<stdio.h>
+int main()
+{
+printf("hola mundo\n");
+printf("Este es el primer programa del curso gg \n");
+}
+ 
+/* terminé de escribir el programa*/
---
+++ semana1/programa_hola.c
@@ -0,0 +1,4 @@
+/*
+Este es un comentario.Me sirve para escribir información importante del programa que estoy haciendo. por ejempolo: Este programa imprime la frase "hola mundo :)"
+creado el o6/08/2018 por irving medina vazquez
+*/
