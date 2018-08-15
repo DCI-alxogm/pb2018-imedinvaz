b767676f5f12388cfc63e93659871114818ed82f
Medina Vazquez Irving <medinavi2018@dci25-187.ugto.mx>
2018-08-15 09:56:05 -0500

    protoejercicios miercoles semana2

---
+++ semana2/edad.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int a,b;
+
+printf("hola, este programa calcula cuantos años tendrás en el 2028\n inserta tu edad: ");
+scanf("%i",&a);
+b= a+10;
+printf("\ntu edad en el 2018 sera de %i años", b);
+return(0);
+}
---
+++ semana2/ejercicio2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+int a,b,c,d,e,f,g,h;
+{
+printf("este programa muestra una serie de operaciones con 4 numeros enteros\n introduce el primer numero: ");
+scanf("%i",&a);
+printf("introduce el segundo numero\n");
+scanf("%i",&b);
+printf("introduce el tercer numero\n");
+scanf("%i",&c);
+printf("introduce el cuarto numero\n");
+scanf("%i",&d);
+
+e = (a+b)*c/d;
+f = ((a+b+c)/d;
+g= (a+b)*c/d;
+h = a+(b*c)/d;
+
+printf ("%i = (%i + %i)*%i/%i\n"e,a,b,c,d);
+return(0);
+}
