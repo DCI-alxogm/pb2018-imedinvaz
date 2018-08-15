d9873d4498ae215bc0b043c5805ba0f44e801e3f
Medina Vazquez Irving <medinavi2018@dci25-187.ugto.mx>
2018-08-15 08:58:46 -0500

    program

---
+++ programa.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int a,b,c;
+int main()
+{ int edad;
+printf("hola, este programa calcula tu edad en ciertos años\n introduce tu edad: \n");
+scanf("%i", &a);
+printf("¿cuantos años en el futuro quieres avanzar?\n");
+scanf ("%i",&b);
+c = (a+b);
+printf("tu edad en %i años será de %i \n De nada!!!! ;U",b,c);
+return(0);
+}
