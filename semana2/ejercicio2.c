4411315b9b012d9ebac78f44aa15ad88675f2a7a
Medina Vazquez Irving <medinavi2018@dci25-175.ugto.mx>
2018-08-16 13:27:18 -0500

    ejercicios semana 2#

--- semana2/ejercicio2.c
+++ semana2/ejercicio2.c
@@ -1,21 +1,38 @@
 #include<stdio.h>
 int main()
-int a,b,c,d,e,f,g,h;
+
+
+
 {
+
+int a;
+int b;
+int c;
+int d;
+float e;
+float f;
+float g;
+float h;
+
+
+
 printf("este programa muestra una serie de operaciones con 4 numeros enteros\n introduce el primer numero: ");
 scanf("%i",&a);
-printf("introduce el segundo numero\n");
+printf("\nintroduce el segundo numero\n");
 scanf("%i",&b);
-printf("introduce el tercer numero\n");
+printf("\nintroduce el tercer numero\n");
 scanf("%i",&c);
-printf("introduce el cuarto numero\n");
+printf("\nintroduce el cuarto numero\n");
 scanf("%i",&d);
 
 e = (a+b)*c/d;
-f = ((a+b+c)/d;
+f = ((a+b*c))/d;
 g= (a+b)*c/d;
 h = a+(b*c)/d;
 
-printf ("%i = (%i + %i)*%i/%i\n"e,a,b,c,d);
+printf ("%f = (%i + %i)*%i/%i \n",e,a,b,c,d);
+printf ("%f = ((%i + %i)*%i)/%i \n",f,a,b,c,d);
+printf ("%f = (%i + %i)*%i/%i \n",g,a,b,c,d);
+printf ("%f = %i + (%i*%i)/%i \n",h,a,b,c,d);
 return(0);
 }
