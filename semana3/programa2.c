7134c85f1247df65be5cecfef009489d42127ebd
Medina Vazquez Irving <medinavi2018@dci25-187.ugto.mx>
2018-08-20 12:55:22 -0500

    programas semana 3 lunes#

--- semana3/programa2.c
+++ semana3/programa2.c
@@ -7,12 +7,12 @@ float x,y,z,t,u;
 float const PI=3.1415926536;
 printf("este programa evalua un valor en diferentes funciones\n inserta el valor");
 scanf("%f", & x);
-y = exp(x);
+y = exp(-x);
 t= x*(PI/180);
 z = cos(t)+2*tan(t/2);
 u = log(x)+3*x*x;
 
-printf("%f = exponente(%f)\n",y,x);
+printf("%f = exponente(-%f)\n",y,x);
 printf("%f = %f rad\n",t,x);
 printf("%f = cos(%f)+2tan(%f/2)\n",z,t,t);
 printf("%f =log(%f)+3(%f)\n",u,x,x);
--- semana3/programa3.c
+++ semana3/programa3.c
@@ -2,7 +2,7 @@
 #include<math.h>
  int main ()
 {
-float a,r,b,x,y,z,u;
+float a,r,b,x,y,z,u,e,d,f;
 printf("este programa convierte coordenadas cartesianas a esfericas y despues la vuelve a convertir a cartesianas\n inserta la coordenada x\n");
 scanf("%f",& x);
 printf("\n inserta la coordenada y\n");
@@ -11,9 +11,15 @@ printf("\n inserta la coordenada z\n");
 scanf("%f", &z);
 r = sqrt(x*x+y*y+z*z);
 u = sqrt(x*x+y*y);
-a = 1/tan(u/z);
-b = 1/tan(y/x);
+a = atan(u/z);
+b = atan(y/x);
 printf("r = %f\na= %f\n b= %f\n",r,a,b);
+
+printf("y al pasarlas a cartesianas de nuevo\n");
+d= r*sin(a)*cos(b);
+e = r*sin(a)*sin(b);
+f = r*cos(a);
+printf("x = %f\ny= %f\n z= %f\n",d,e,f);
 
 return(0);
 }
