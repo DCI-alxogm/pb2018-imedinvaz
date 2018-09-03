49076968209969baa03a7f2c7d9abf2b8d5491be
Medina Vazquez Irving <medinavi2018@dci25-187.ugto.mx>
2018-09-03 15:51:10 -0500

    lunes semana 5#	modified:   semana5/ejem1conwhile.c

--- semana5/ejem1conwhile.c
+++ semana5/ejem1conwhile.c
@@ -3,7 +3,24 @@
 int main()
 {
 	float temp_c, temp_k;
-	float Temp_c=100, final=200,delta;
-	int n=10, i;
+	float Temp_c, final,delta;
+	int n,i;
+	char cd[2]
 
-	delta
+	cd[0]='s';
+	printf("inserta el número de pasos\n");
+	scanf("%i",&n);
+	printf("inserta la temperatura inicial\n");
+	scanf("%f",&Temp_c);
+	printf("inserta la temperatura final\n");
+	scanf("%f",&final);
+
+	delta=(final-Temp_c)/n;
+	for(i=0;i<n;i++)
+	{
+		temp_k=temp_c+273.15;
+		printf("%f %f\n",temp_c,temp_k);
+		temp_c=temp_c+delta;
+	}
+return 0;
+}
