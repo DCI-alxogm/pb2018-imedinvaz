a1c04a561444ae01e125013c606ed3ea581d9eed
Medina Vazquez Irving <medinavi2018@dci25-152.ugto.mx>
2018-09-17 09:56:04 -0500

    semana7#

--- semana7/ejer1.c
+++ semana7/ejer1.c
@@ -3,12 +3,30 @@
 
 int main()
 {
-	float P=0,a=0,b=0,N=0,k=0;
+	float P=0,a=0,b=0,N=0,k=0,x,I=0;
 	char linea[255];
 	FILE *entrada;
 	FILE *salida;
 	entrada= fopen("______","r");
 	fgets(linea,255,(FILE*)estadistica);
+	fscanf(entrada,"%f%f%f%f%f",&P,&a,&b,&N,&k);
+	f=(b-a)/N //f es el valor en aumento que tiene que tomar para llegar al limite deseado
+	e=a; // no modificar a
+	for(c=0,c<N,c++);
+	{
+	e=e+f;
+	d=pow(e,P);	
+	h=(e-a)/k;
+	h*(d+a)/2;
+	j= //valor para que llegue  K-1 
+	for(i=0,i=j,i++);
+	{
+
+
+	}
+	}
+	
+	
 	
 
 
