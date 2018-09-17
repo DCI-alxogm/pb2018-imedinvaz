12006b4bc7c12a7a159e8ef658fcc1894d7bec9d
Medina Vazquez Irving <medinavi2018@dci25-187.ugto.mx>
2018-09-17 08:41:44 -0500

    semana7#

---
+++ semana7/ejer1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stlib.h>
+
+int main()
+{
+	float P=0,a=0,b=0,N=0,k=0;
+	char linea[255];
+	FILE *entrada;
+	FILE *salida;
+	entrada= fopen("______","r");
+	fgets(linea,255,(FILE*)estadistica);
+	
+
+
+
+return 0;
+}
